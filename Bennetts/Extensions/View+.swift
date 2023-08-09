ba0fc97c5ef95bcc5c2a3cda120137188b7a2ee2
Olgu SIRMAN <olgu.sirman@kobil.com>
2023-08-09 15:06:14 +0100

    Configure contentDescription view

---
+++ Bennetts/Extensions/View+.swift
@@ -0,0 +1,41 @@
+//
+//  View+.swift
+//  Bennetts
+//
+//  Created by Olgu SIRMAN on 09/08/2023.
+//
+
+import SwiftUI
+
+struct DeviceRotationViewModifier: ViewModifier {
+    let action: (UIDeviceOrientation) -> Void
+
+    func body(content: Content) -> some View {
+        content
+            .onAppear()
+            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
+                action(UIDevice.current.orientation)
+            }
+    }
+}
+
+extension View {
+    /// Applies the given transform if the given condition evaluates to `true`.
+    /// - Parameters:
+    ///   - condition: The condition to evaluate.
+    ///   - transform: The transform to apply to the source `View`.
+    /// - Returns: Either the original `View` or the modified `View` if the condition is `true`.
+    @ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
+        if condition {
+            transform(self)
+        } else {
+            self
+        }
+    }
+}
+
+extension View {
+    func onRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
+        self.modifier(DeviceRotationViewModifier(action: action))
+    }
+}
---
+++ Bennetts/View/ImageContentDescriptionView.swift
@@ -0,0 +1,40 @@
+//
+//  ImageContentDescriptionView.swift
+//  Bennetts
+//
+//  Created by Olgu SIRMAN on 09/08/2023.
+//
+
+import SwiftUI
+
+struct ImageContentDescriptionView: View {
+
+    var userDisplayName: String
+    var userProfileImage: URL?
+    var likeCount: Int
+
+    @State private var orientation = UIDeviceOrientation.unknown
+
+    private var isLandscape: Bool {
+        orientation == .landscapeLeft || orientation == .landscapeRight
+    }
+
+    var body: some View {
+        HStack() {
+            ProfileImageView(name: userDisplayName, photoUrl: userProfileImage)
+            Spacer()
+            LikeCountView(likeCount: likeCount)
+        }
+        .padding()
+        .onRotate { newOrientation in
+            orientation = newOrientation
+        }
+        .if(isLandscape) { view in
+            view.background(Material.regular)
+        }
+    }
+}
+
+#Preview {
+    ImageContentDescriptionView(userDisplayName: "Olgu SIRMAN", userProfileImage: .sampleProfileImage, likeCount: 14233)
+}
