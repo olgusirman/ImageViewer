7e4b80e84f8c4cc9a34fa4ab92a2fbc44963c7d3
Olgu SIRMAN <olgu.sirman@kobil.com>
2023-08-09 15:05:14 +0100

    Update sample images

--- Bennetts/Extensions/URL+.swift
+++ Bennetts/Extensions/URL+.swift
@@ -9,6 +9,6 @@ import Foundation
 
 extension URL {
 
-    static let sampleImage = URL(string: "https://images.unsplash.com/photo-1661956600654-edac218fea67?ixid=M3wzNzYyMzV8MXwxfGFsbHwxfHx8fHx8Mnx8MTY5MTUwMjA5OHw%5Cu0026ixlib=rb-4.0.3")
-
+    static let sampleImage = URL(string: "https://images.unsplash.com/photo-1682685797703-2bb22dbb885b?crop=entropy%5Cu0026cs=tinysrgb%5Cu0026fit=max%5Cu0026fm=jpg%5Cu0026ixid=M3wzNzYyMzV8MXwxfGFsbHwxfHx8fHx8Mnx8MTY5MTUwNzAwNHw%5Cu0026ixlib=rb-4.0.3%5Cu0026q=80%5Cu0026w=400")
+    static let sampleProfileImage = URL(string: "https://images.unsplash.com/profile-1609545740442-928866556c38image?ixlib=rb-4.0.3%5Cu0026crop=faces%5Cu0026fit=crop%5Cu0026w=32%5Cu0026h=32")
 }
