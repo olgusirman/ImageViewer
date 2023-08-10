2f8597a7f9bb58194cd1e6cbc348ac6645120920
Olgu SIRMAN <olgu.sirman@kobil.com>
2023-08-10 20:43:33 +0100

    Implement endpoint type tests

---
+++ BennettsTests/UnsplashEndpointTypeTests.swift
@@ -0,0 +1,42 @@
+//
+//  UnsplashEndpointTypeTests.swift
+//  BennettsTests
+//
+//  Created by Olgu SIRMAN on 10/08/2023.
+//
+
+import XCTest
+@testable import Bennetts
+
+final class UnsplashEndpointTypeTests: XCTestCase {
+
+    var stub: UnsplashEndpointType!
+
+    override func setUpWithError() throws {
+        stub = .photos
+    }
+
+    override func tearDownWithError() throws {
+        stub = nil
+    }
+
+    func testEndpointType_whenConfigureWithoutQueryItems_expectCorrectUrl() async throws {
+
+        // when
+        let components = stub.makeComponent(queryItems: nil)
+
+        // then
+        XCTAssertEqual(components.url?.absoluteString, "https://api.unsplash.com/photos/")
+    }
+
+    func testEndpointType_whenConfigureWithQueryItems_expectCorrectUrl() async throws {
+
+        let queryItems = [URLQueryItem(name: "client_id", value: "1234")]
+
+        // when
+        let components = stub.makeComponent(queryItems: queryItems)
+
+        // then
+        XCTAssertEqual(components.url?.absoluteString, "https://api.unsplash.com/photos/?client_id=1234")
+    }
+}
